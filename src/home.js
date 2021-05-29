a263c2fc24fcc6538384a659a9d0e5897c7076ec
susiyanti200 <susiyantig11@gmail.com>
2021-05-29 15:57:21 +0700

    Add h1 to home content

--- src/home.js
+++ src/home.js
@@ -5,6 +5,9 @@ const generateHomeContent = () => {
   const hero = document.createElement("div");
   hero.className = "hero";
 
+  const h1 = document.createElement("h1");
+  h1.textContent = "All YOU CAN EAT BUFFET";
+
   const p = document.createElement("p");
   p.textContent =
     "Enjoy our current offering of delicious dishes, made with local ingredients sourced from our friends and neighbors.";
@@ -12,7 +15,7 @@ const generateHomeContent = () => {
   const button = document.createElement("button");
   button.textContent = "View our menu";
 
-  hero.append(p, button);
+  hero.append(h1, p, button);
   main.appendChild(hero);
   return main;
 };
