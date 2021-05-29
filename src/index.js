ad9307dfe58066608bac3ef98aa006dd5931f7fc
susiyanti200 <susiyantig11@gmail.com>
2021-05-29 14:43:58 +0700

    Add nav switching logic

--- src/index.js
+++ src/index.js
@@ -1,16 +1,36 @@
 import "./style.css";
 import { generateHeader, toggleActiveNav } from "./header";
 import generateHomeContent from "./home";
+import generateMenuContent from "./menu";
 import generateContactContent from "./contact";
 
 const content = document.querySelector("#content");
 
 const navList = ["home", "menu", "contact"];
 const header = generateHeader("FOODIE", navList);
+const navEl = header.querySelector("nav");
+
 content.append(header);
 toggleActiveNav(navList[0]);
 
-// const homeContent = generateHomeContent();
-// content.append(homeContent);
+const homeContent = generateHomeContent();
+const menuContent = generateMenuContent();
 const contactContent = generateContactContent();
-content.append(contactContent);
+content.append(homeContent);
+
+const contentList = {
+  home: homeContent,
+  menu: menuContent,
+  contact: contactContent,
+};
+
+navEl.addEventListener("click", (e) => {
+  for (const nav of navList) {
+    if (e.target.parentElement.id === `nav-${nav}`) {
+      content.removeChild(content.lastElementChild);
+      content.append(contentList[nav]);
+      toggleActiveNav(nav);
+      navEl.classList.add("nav-close");
+    }
+  }
+});
